a54246238d3655251e70e8f98d02e02305438e11
TheSensif <sergisimon7@gmail.com>
2023-10-03 19:33:53 +0200

    feat/stacks: popover

--- stacks/lib/app_data.dart
+++ stacks/lib/app_data.dart
@@ -6,6 +6,7 @@ import 'package:flutter/services.dart';
 class AppData with ChangeNotifier {
   bool isSidebarLeftVisible = false;
   bool isSidebarRightVisible = false;
+  Color isColor = Colors.black;
 
   void toggleSidebarLeft() {
     isSidebarLeftVisible = !isSidebarLeftVisible;
@@ -15,5 +16,22 @@ class AppData with ChangeNotifier {
   void toggleSidebarRight() {
     isSidebarRightVisible = !isSidebarRightVisible;
     notifyListeners();
+  }
+
+  void changeColor(String color) {
+    switch (color) {
+      case "black":
+        isColor = Colors.black;
+        break;
+      case "red":
+        isColor = Colors.red;
+        break;
+      case "green":
+        isColor = Colors.green;
+        break;
+      case "blue":
+        isColor = Colors.blue;
+        break;
+    }
   }
 }
--- stacks/lib/layout_part_central.dart
+++ stacks/lib/layout_part_central.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'app_data.dart';
+import 'widget_popover.dart';
 
 class LayoutPartCentral extends StatefulWidget {
   const LayoutPartCentral({super.key});
@@ -10,6 +12,9 @@ class LayoutPartCentral extends StatefulWidget {
 }
 
 class LayoutPartCentralState extends State<LayoutPartCentral> {
+  final GlobalKey _settingsButtonKey = GlobalKey();
+  Color lastTextColor = Colors.black;
+
   @override
   Widget build(BuildContext context) {
     AppData appData = Provider.of<AppData>(context);
@@ -36,7 +41,60 @@ class LayoutPartCentralState extends State<LayoutPartCentral> {
             )
           ],
         ),
-        middle: const Text("hola"),
+        middle: CupertinoButton(
+          key: _settingsButtonKey,
+          padding: const EdgeInsets.all(0.0),
+          onPressed: () {
+            WidgetPopover.showPopover(
+                context: context,
+                key: _settingsButtonKey,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          appData.changeColor("black");
+                          updateLastTextColor(appData.isColor);
+                          WidgetPopover.hidePopover();
+                        },
+                        child: const Text('Negre'),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          appData.changeColor("red");
+                          updateLastTextColor(appData.isColor);
+                          WidgetPopover.hidePopover();
+                        },
+                        child: const Text('Vermell'),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          appData.changeColor("green");
+                          updateLastTextColor(appData.isColor);
+                          WidgetPopover.hidePopover();
+                        },
+                        child: const Text('Verd'),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          appData.changeColor("blue");
+                          updateLastTextColor(appData.isColor);
+                          WidgetPopover.hidePopover();
+                        },
+                        child: const Text('Blau'),
+                      ),
+                    ],
+                  ),
+                ));
+          },
+          child: const Icon(
+            CupertinoIcons.settings,
+            color: CupertinoColors.black,
+            size: 24.0,
+            semanticLabel: 'Text to announce in accessibility modes',
+          ),
+        ),
         trailing: Row(
           mainAxisSize: MainAxisSize.min,
           children: [
@@ -58,13 +116,24 @@ class LayoutPartCentralState extends State<LayoutPartCentral> {
           ],
         ),
       ),
-      child: const Padding(
+      child: Padding(
         padding: EdgeInsets.only(top: 50.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [Text("Central")],
+          children: [
+            Text(
+              "Central",
+              style: TextStyle(color: appData.isColor),
+            )
+          ],
         ),
       ),
     );
+  }
+
+  void updateLastTextColor(Color newColor) {
+    setState(() {
+      lastTextColor = newColor;
+    });
   }
 }
